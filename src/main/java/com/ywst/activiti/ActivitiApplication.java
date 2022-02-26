51153ec09351254ccc9bec64ab2b7c3f1e876e3a
gaoxiulong <13913036611@139.com>
2022-02-26 22:09:18 +0800

    springboot整合activiti7测试实例

---
+++ src/main/java/com/ywst/activiti/ActivitiApplication.java
@@ -0,0 +1,14 @@
+package com.ywst.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiApplication.class, args);
+		System.out.println("系统启动成功！");
+	}
+
+}
